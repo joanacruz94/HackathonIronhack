ca0adff6965e85782f691a361128f6289837355d
Joana Cruz <joanamartadacruz@gmail.com>
2020-03-06 11:56:54 +0000

    form submission done, everything working

--- src/components/OurForm/index.jsx
+++ src/components/OurForm/index.jsx
@@ -7,6 +7,38 @@ class OurForm extends Component {
     constructor(){
         super();
         this.idInput = 0;
+        this.state = {
+            q1: '',
+            q2: '',
+            q3: '',
+            q4: '',
+            q5: '',
+            q6: '',
+            q7: '',
+            q8: '',
+            q9: '',
+            q10: '',
+            q11: '',
+            q12: '',
+            q13: '',
+            q14: '',
+            q15: '',
+            q16: '',
+            q17: true,
+            q18: true,
+            q19: true,
+            q20: true,
+            q21: true,
+            q22: true,
+            q23: true,
+            q24: true,
+            q25: true,
+            q26: true,
+            q27: true
+        }
+        this.keys = this.createKeys();
+        this.changeAnswersState = this.changeAnswersState.bind(this);
+        this.handleSubmission = this.handleSubmission.bind(this);
     }
 
     incrementId(){
@@ -14,27 +46,39 @@ class OurForm extends Component {
         return this.idInput;
     }
 
-    createKeys(elements){
-        return elements.map((x) => {return this.incrementId()});
+    changeAnswersState(inputName, value){
+        this.setState(previousState => {
+            return {
+                [inputName]: value
+            }
+        })
+    }
+
+    handleSubmission(){
+        const submission = {...this.state};
+
+        console.log(submission);
+    }
+
+    createKeys(){
+        let array = [];
+        for(let i = 1; i < 28; i++) array.push("q" + i);
+        return array;
     }
 
     render() {
         const { dataForm } = this.props;
         const elements = dataForm[0].pages[0].elements;
-        const keys = this.createKeys(elements);
-        console.log(keys);
 
         return (
             <div>
                 <Form controlId="formBasic">
                     {
                         elements.map((element, index)=> (
-                            <Fragment >
-                                <OurInput {...element} idInput={keys[index]}/>
-                            </Fragment>
+                            <OurInput {...element} idInput={this.keys[index]} answersState={(inputName, value) => this.changeAnswersState(inputName, value)}/>
                         ))
                     }
-                <Button controlId="buttonSubmit" variant="primary" type="submit">Submit</Button>
+                <Button controlId="buttonSubmit" variant="primary" onClick={this.handleSubmission}>Submit</Button>
                 </Form>
             </div>
         )
